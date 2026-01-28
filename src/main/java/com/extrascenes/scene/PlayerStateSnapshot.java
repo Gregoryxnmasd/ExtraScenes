5ef2341d4096a1d0e1b11b5c0beaaf31750a08f9
Palomero33 <101456667+Gregoryxnmasd@users.noreply.github.com>
2026-01-28 22:10:08 +0200

    Avoid deprecated Attribute enum accessors

--- src/main/java/com/extrascenes/scene/PlayerStateSnapshot.java
+++ src/main/java/com/extrascenes/scene/PlayerStateSnapshot.java
@@ -2,12 +2,16 @@ package com.extrascenes.scene;
 
 import java.util.Collection;
 import org.bukkit.GameMode;
+import org.bukkit.NamespacedKey;
+import org.bukkit.Registry;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 
 public class PlayerStateSnapshot {
+    private static final NamespacedKey GENERIC_MAX_HEALTH_KEY = NamespacedKey.minecraft("generic.max_health");
+    private static final NamespacedKey LEGACY_MAX_HEALTH_KEY = NamespacedKey.minecraft("max_health");
     private final GameMode gameMode;
     private final boolean flying;
     private final float flySpeed;
@@ -30,12 +34,11 @@ public class PlayerStateSnapshot {
     }
 
     private static Attribute resolveMaxHealthAttribute() {
-        for (Attribute attribute : Attribute.values()) {
-            if ("GENERIC_MAX_HEALTH".equals(attribute.name()) || "MAX_HEALTH".equals(attribute.name())) {
-                return attribute;
-            }
+        Attribute attribute = Registry.ATTRIBUTE.get(GENERIC_MAX_HEALTH_KEY);
+        if (attribute != null) {
+            return attribute;
         }
-        return null;
+        return Registry.ATTRIBUTE.get(LEGACY_MAX_HEALTH_KEY);
     }
 
     public GameMode getGameMode() {
